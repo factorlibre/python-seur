9a1cdbb6d8763b5ccf52aba67e59e846c13077a3
zahra <zahra.velasco@factorlibre.com>
2019-11-28 16:20:42 +0100

    añadiendo try en las recogidas

--- seur/picking.py
+++ seur/picking.py
@@ -222,9 +222,14 @@ class Picking(API):
 
         result = self.connect(url, xml)
         dom = parseString(result)
-        info = parseString(
-            dom.getElementsByTagName('out')[0].childNodes[0].data.
-            encode('utf-8'))
+        try:
+            info = parseString(
+                dom.getElementsByTagName('out')[0].childNodes[0].data.
+                encode('utf-8'))
+        except:
+            info = parseString(
+                dom.getElementsByTagName('ns1:out')[0].childNodes[0].data.
+                encode('utf-8'))
         pickup_ref = False
         pickup_num = False
         amount = False
